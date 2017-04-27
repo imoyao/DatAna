9d85f14c814ebea46981e39854398b02fcc86cc4
imoyao <emailme8@163.com>
2017-04-27 18:08:33 +0800

    try to add time_it() to read_excel().

--- figtest.py
+++ figtest.py
@@ -3,42 +3,88 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import mlab
 from matplotlib import rcParams
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+'''
+http://old.sebug.net/paper/books/scipydoc/index.html
+http://blog.csdn.net/jenyzhang/article/details/52047557
+http://matplotlib.org/index.html
+不显示图片，以便进行批处理：
 
+import matplotlib
+matplotlib.use('Agg')
+需加在 import matplotlib.pyplot as plt 之前，同时删掉plt.show()
+'''
 
 """
 Simple demo of a horizontal bar chart.
+
+atplotlib.pyplot.bar(left, height, width=0.8, bottom=None, hold=None, data=None, **kwargs)
+参数说明：
+
+left: 每一个柱形左侧的X坐标
+
+height:每一个柱形的高度
+
+width: 柱形之间的宽度
+
+bottom: 柱形的Y坐标
+
+color: 柱形的颜色
+
+the count 彭公镇 in data is:1723
+the count 亭口镇 in data is:1324
+the count 相公镇 in data is:1309
+the count 巨家镇 in data is:835
+the count 昭仁街道办 in data is:1543
+the count 枣园镇 in data is:778
+the count 丁家镇 in data is:584
+the count 洪家镇 in data is:1419
 """
 
+# X=[0,1,2,3,4,5]  
+# Y=[222,42,455,664,454,334]    
+# fig = plt.figure()  
+# plt.bar(X,Y,0.4,color="green")  
+# plt.xlabel("X-axis")  
+# plt.ylabel("Y-axis")  
+# plt.title("bar chart")  
+
+import matplotlib.font_manager as mfm
+show_font = mfm.FontProperties(fname='./fonts/msyh.ttf') #指定默认字体  
+# x = [0,1,2,3,4,5,6,7]
+x = np.arange(0,8,1)
+# x = np.linspace(0, 1, 12)
+x_axis= ('彭公镇','亭口镇','相公镇','巨家镇','昭仁街道办','枣园镇','丁家镇','洪家镇')
+y = [1723,1324,1309,835,1543,778,584,1419]
+
+fig = plt.figure(figsize=(10,8))
+rects = plt.bar(x,y,0.8,facecolor = 'lightskyblue',edgecolor = 'white')
+plt.xlabel(u"乡镇名",fontproperties=show_font)  
+plt.ylabel(u"贫困户数量(户)",fontproperties=show_font)  
+plt.title(u"长武县贫困户信息图",fontproperties=show_font,fontsize=20)
+
+# for x_text,y_text in x,y:
+# 	plt.text(x-0.02,y,str(int(x)))
 
 
-plt.rcdefaults()
-fig, ax = plt.subplots()
 
-# Example data
-people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
-y_pos = np.arange(len(people))
-print y_pos
-np_ran = np.random.rand(len(people))
-print type(np_ran)
-for i in np_ran:
-	print i
-performance = 10 * np_ran
-print performance
-error = np.random.rand(len(people))
+def autolabel(rects):
+    for rect in rects:
+        height = rect.get_height()
+        print height
+        x = rect.get_x()+rect.get_width()/2.0
+        y = 1.02*height
 
-ax.barh(y_pos, performance, xerr=error, align='center',
-        color='green', ecolor='black')
-ax.set_yticks(y_pos)
-ax.set_yticklabels(people)
-ax.invert_yaxis()  # labels read top-to-bottom
-ax.set_xlabel('Performance')
-ax.set_title('How fast do you want to go today?')
+        # s = "wosho %.2f"%(0.98)
+        percent = "%.2f"%((int(height)/9515.0)*100)
 
-plt.show()
+        # percent = "%.2f"%(height/9515.0)*100
+        s = "  "+str(int(height))+"\n"+"("+str(percent)+"%"+")"
+        plt.text(x-0.3, y, s)
+        # print x-0.02,y,s
+autolabel(rects)
+plt.xticks(x,x_axis,fontproperties=show_font, fontsize=8)
+plt.show()   
 
-
-# fig1 = plt.figure(2)
-# rects =plt.bar(left = (0.2,1),height = (1,0.5),width = 0.2,align="center",yerr=0.000001)
-# plt.title('')
-# ax.set_title('How fast do you want to go today?')
-# plt.show()
--- test.py
+++ test.py
@@ -1,43 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''
-http://www.cnblogs.com/lhj588/archive/2012/01/06/2314181.html
-http://www.cnblogs.com/jiangzhaowei/p/5856617.html
-http://blog.csdn.net/seetheworld518/article/details/49536599
-'''
 
-import time
-import xlrd
-import collections
-import sys
-reload(sys)
-sys.setdefaultencoding("utf-8")
+how to???
+def foo():
+	listmylist.append()
+	return mylist
 
-
-fname = "cw.xlsx"
-fname1 = "china.xls"
-work_book = xlrd.open_workbook(fname)    #type=list
-sheet_name = work_book.sheet_names()[0].encode("utf-8") #sheet name
-# print sheet_name
-table = work_book.sheet_by_index(0)  #读取第一个sheet   
-table_list = table.col_values(1)    #读取第二列（index=1）
-
-useful_data_list = table_list[5:-1]
-# print useful_data_list
-
-# countyname = table.cell_value(5,1).encode('utf-8')
-# county = table.cell(3,1).value.encode('utf-8')
-# countype = table.cell(3,1).ctype
-# #ctype : 0 empty,1 string, 2 number, 3 date, 4 boolean, 5 error
-# print countyname
-# print county
-# print countype
-data = collections.Counter(useful_data_list)
-count_list = []
-for k in data:
-    print "the count {} in data is:{}".format(k,data[k])
-    count_list.append(data[k])
-    print sum(count_list)
-
+a = foo()
+type(a) = list
 
 
